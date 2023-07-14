3db6510419a6f4b0d6e00b223ac6f159c5623d9a
Denloob <den@denloob.uk>
2023-07-14 22:06:38 +0300

    Add simple game loop

--- src/main.c
+++ src/main.c
@@ -1,8 +1,41 @@
+#include "SDL.h"
 #include "main.h"
+#include "renderer.h"
 #include <stdlib.h>
 
 int main(void)
 {
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    if (initSDL(&window, &renderer, WINDOW_NAME, WINDOW_WIDTH, WINDOW_HEIGHT))
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "initSDL()",
+                                 SDL_GetError(), 0);
+        SDL_Log("initSDL(): %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    while (1)
+    {
+        SDL_SetRenderDrawColor(renderer, BACKGROUND_COLOR);
+        SDL_RenderClear(renderer);
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                SDL_DestroyWindow(window);
+                SDL_DestroyRenderer(renderer);
+                return EXIT_SUCCESS;
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(FRAME_DURATION);
+    }
 
     return EXIT_SUCCESS;
 }
--- src/main.h
+++ src/main.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define WINDOW_NAME "Game"
+
+#define BACKGROUND_COLOR 0xFF, 0xFF, 0xFF, 0xFF
+
+#define FPS 60
+
+#define FRAME_DURATION (1000 / FPS)
