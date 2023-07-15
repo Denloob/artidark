9ed153f1f431b66b4f5e72f31a85cae3ac1e1323
Denloob <den@denloob.uk>
2023-07-15 12:22:26 +0300

    Change the applyVelocity function to move, so it also makes sure that the character doesn't clip through tiles

--- src/character.c
+++ src/character.c
@@ -1,23 +1,53 @@
 #include "SDL.h"
 #include "character.h"
+#include "tile.h"
+#include "vec.h"
+#include <stddef.h>
 
 void character_draw(const Character *character, SDL_Renderer *renderer)
 {
     SDL_RenderDrawRectF(renderer, &character->hitbox);
 }
 
-void character_tick(Character *character)
+void character_tick(Character *character, const VecTile tiles)
 {
-    character_applyVelocity(character);   
+    character_move(character, tiles);
 }
 
-void character_applyVelocity(Character *character)
+void character_move(Character *character, const VecTile tiles)
 {
     character->hitbox.x += character->velocity.x;
+
+    character_fixTileClip(character, tiles, x, w);
+
     character->hitbox.y += character->velocity.y;
+
+    character_fixTileClip(character, tiles, y, h);
 }
 
 void character_applyGravity(Character *character, float gravity)
 {
     character->velocity.y += gravity;
+}
+
+VecTile *character_findCollisions(const Character *character,
+                                  const VecTile tiles)
+{
+    VecTile *collisions = vector_create();
+
+    for (size_t i = 0; i < vector_size(tiles); i++)
+    {
+        if (tiles[i].solid &&
+            SDL_HasIntersectionF(&character->hitbox, &tiles[i].hitbox))
+        {
+            // We need the no-lint because vector_add uses sizeof, and as our
+            // vector is vector of pointers, so we do sizeof pointer, the
+            // sizeof is marked as bugprone sizeof expression
+
+            // NOLINTNEXTLINE(bugprone-sizeof-expression)
+            vector_add(&collisions, &tiles[i]);
+        }
+    }
+
+    return collisions;
 }
--- src/character.h
+++ src/character.h
@@ -1,12 +1,39 @@
 #pragma once
 
 #include "SDL.h"
+#include "tile.h"
+#include "vec.h"
 
 typedef struct Character
 {
     SDL_FRect hitbox;
     SDL_FPoint velocity;
 } Character;
+
+/**
+ * @brief Fixes character clipping through tile on one axis.
+ *
+ * @param tiles Tiles the character could be possibly clipped through.
+ * @param posAxis `x` or `y`
+ * @param sizeAxis `w` or `h`
+ */
+#define character_fixTileClip(character, tiles, posAxis, sizeAxis)             \
+    {                                                                          \
+        VecTile *collisions = character_findCollisions(character, tiles);      \
+        float direction = (character->velocity).posAxis;                       \
+                                                                               \
+        for (size_t i = 0; i < vector_size(collisions); i++)                   \
+            if (direction < 0)                                                 \
+                (character->hitbox).posAxis =                                  \
+                    (collisions[i]->hitbox).posAxis +                          \
+                    (collisions[i]->hitbox).sizeAxis;                          \
+            else if (direction > 0)                                            \
+                (character->hitbox).posAxis =                                  \
+                    (collisions[i]->hitbox).posAxis -                          \
+                    (character->hitbox).sizeAxis;                              \
+                                                                               \
+        vector_free(collisions);                                               \
+    }
 
 /**
  * @brief Draws the character.
@@ -17,13 +44,18 @@ void character_draw(const Character *character, SDL_Renderer *renderer);
 
 /**
  * @brief Updates character's fields.
+ *
+ * @param tiles The tiles surrounding the character.
  */
-void character_tick(Character *character);
+void character_tick(Character *character, const VecTile tiles);
 
 /**
- * @brief Applies the character's velocity to it's position.
+ * @brief Moves the character as needed and checks that the character doesn't
+ *          clip through tiles.
+ *
+ * @param tiles The tiles surrounding the character.
  */
-void character_applyVelocity(Character *character);
+void character_move(Character *character, const VecTile tiles);
 
 /**
  * @brief Applies gravity to the character's velocity.
@@ -31,3 +63,12 @@ void character_applyVelocity(Character *character);
  * @param gravity The acceleration of gravity.
  */
 void character_applyGravity(Character *character, float gravity);
+
+/**
+ * @brief Finds the collisions between the character and the tiles.
+ *
+ * @param tiles The tiles to find collisions with.
+ * @return Vector of pointers to all the tiles player collides with.
+ */
+VecTile *character_findCollisions(const Character *character,
+                                  const VecTile tiles);
