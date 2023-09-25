82aa326c727e3f020020ec85bb15d13ad0fbd21e
Denloob <den@denloob.uk>
2023-09-25 10:15:10 +0300

    Add keyboard key event system
    Tiles can subscribe to keys, and their callback will be called when this key is pressed.

---
+++ src/tile_keyboard_events.c
@@ -0,0 +1,109 @@
+#include "hashmap.h"
+#include "tile_keyboard_events.h"
+#include "utils.h"
+#include "vec.h"
+
+static size_t tile_keyboard_hash_SDL_Keycode(const SDL_Keycode *key)
+{
+    return hashmap_hash_default(key, sizeof(*key));
+}
+
+static int tile_keyboard_compare_SDL_Keycode(const SDL_Keycode *a,
+                                             const SDL_Keycode *b)
+{
+    return memcmp(a, b, sizeof(*a));
+}
+
+static SDL_Keycode *tile_keyboard_copy_SDL_Keycode(const SDL_Keycode *key)
+{
+    SDL_Keycode *key_dup = xmalloc(sizeof(*key));
+    memcpy(key_dup, key, sizeof(*key));
+    return key_dup;
+}
+
+static void tile_keyboard_free_SDL_Keycode(SDL_Keycode *key)
+{
+    free(key);
+}
+
+KeyEventSubscribers *tile_keyboard_events_create()
+{
+    KeyEventSubscribers *subscribers = xmalloc(sizeof(*subscribers));
+    hashmap_init(subscribers, tile_keyboard_hash_SDL_Keycode,
+                 tile_keyboard_compare_SDL_Keycode);
+    hashmap_set_key_alloc_funcs(subscribers, tile_keyboard_copy_SDL_Keycode,
+                                tile_keyboard_free_SDL_Keycode);
+
+    return subscribers;
+}
+
+void tile_keyboard_events_destroy(KeyEventSubscribers *subscribers)
+{
+    vec_TileCallback subscribed_callbacks;
+
+    hashmap_foreach_data(subscribed_callbacks, subscribers)
+    {
+        vector_foreach(it, subscribed_callbacks)
+        {
+            TileCallback *callback = *it;
+            free(callback);
+        }
+
+        vector_free(subscribed_callbacks);
+    }
+
+    hashmap_cleanup(subscribers);
+    free(subscribers);
+}
+
+void tile_keyboard_events_subscribe_dynamic(KeyEventSubscribers *subscribers,
+                                            SDL_Keycode key,
+                                            TileCallback *callback)
+{
+    vec_TileCallback subscribed_callbacks = hashmap_remove(subscribers, &key);
+    if (!subscribed_callbacks)
+    {
+        subscribed_callbacks = vector_create();
+    }
+
+    // NOLINTNEXTLINE(bugprone-sizeof-expression)
+    vector_add(&subscribed_callbacks, callback);
+
+    hashmap_put(subscribers, &key, subscribed_callbacks);
+}
+
+void tile_keyboard_events_unsubscribe(KeyEventSubscribers *subscribers,
+                                      SDL_Keycode key, TileCallback *callback)
+{
+    vec_TileCallback subscribed_callbacks = hashmap_remove(subscribers, &key);
+
+    if (!subscribed_callbacks)
+        return;
+
+    for (size_t i = 0; i < vector_size(subscribed_callbacks); i++)
+    {
+        if (subscribed_callbacks[i] == callback)
+        {
+            vector_remove(&subscribed_callbacks, i);
+            break;
+        }
+    }
+
+    hashmap_put(subscribers, &key, subscribed_callbacks);
+}
+
+void tile_keyboard_events_notify(KeyEventSubscribers *subscribers,
+                                 SDL_Keycode key)
+{
+    TileCallback **subscribed_callbacks = hashmap_get(subscribers, &key);
+
+    if (!subscribed_callbacks)
+        return;
+
+    vector_foreach(it, subscribed_callbacks)
+    {
+        TileCallback *callback = *it;
+
+        callback->func(callback->args);
+    }
+}
---
+++ src/tile_keyboard_events.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "SDL.h"
+#include "hashmap.h"
+#include "tile_callback.h"
+#include "tileset.h"
+#include "utils.h"
+
+/**
+ * @brief Convenience macro to subscribe a tile type (textureID) from a
+ *          tileset to a key.
+ *
+ * @param subscribers (KeyEventSubscribers *)
+ * @param key (SDL_Keycode) The event keycode to subscribe to.
+ * @param tileset (const Tileset *) The tileset from which to query the TileCallback.
+ * @param textureID (int) The textureID for which to look up the TileCallback.
+ *
+ * @see tile_keyboard_events_subscribe_dynamic
+ * @see tile_keyboard_events_unsubscribe
+ */
+#define tile_keyboard_events_subscribe(subscribers, key, tileset, textureID)   \
+    do                                                                         \
+    {                                                                          \
+        TileCallback *tileCallback = xmalloc(sizeof(*tileCallback));           \
+        tileset_QueryTextureByID(tileset, textureID, NULL, NULL,               \
+                                 tileCallback);                                \
+        tile_keyboard_events_subscribe_dynamic(subscribers, key,               \
+                                               tileCallback);                  \
+    } while (0)
+
+typedef TileCallback **vec_TileCallback;
+
+typedef HASHMAP(SDL_Keycode, TileCallback *) KeyEventSubscribers;
+
+/**
+ * @brief Creates a new map for keyboard events subscribers.
+ *
+ * @return A new map for keyboard events subscribers.
+ *
+ * @see tile_keyboard_events_destroy
+ */
+KeyEventSubscribers *tile_keyboard_events_create();
+
+/**
+ * @brief Destroys a map for keyboard events subscribers and all the
+ *          subscribers. Also frees all the subscribed TileCallbacks (doesn't
+ *          cleanup, only free).
+ *
+ * @see tile_keyboard_events_create
+ */
+void tile_keyboard_events_destroy(KeyEventSubscribers *subscribers);
+
+/**
+ * @brief Subscribe to keyboard events.
+ *
+ * @param key The event keycode to subscribe to.
+ * @param callback Dynamically allocated struct of the subscriber's callback
+ *                  (managed by the subscriber list until unsubscribed.
+ *                      Will be freed at destruction).
+ *
+ * @see tile_keyboard_events_unsubscribe
+ * @see tile_keyboard_events_destroy
+ */
+void tile_keyboard_events_subscribe_dynamic(KeyEventSubscribers *subscribers,
+                                            SDL_Keycode key,
+                                            TileCallback *callback);
+
+/**
+ * @brief Unsubscribe from keyboard events.
+ *
+ * @param key The event keycode to unsubscribe from.
+ * @param callback The callback of the subscriber (the exact same pointer used
+ *                  for subscription).
+ *
+ * @see tile_keyboard_events_subscribe
+ */
+void tile_keyboard_events_unsubscribe(KeyEventSubscribers *subscribers,
+                                      SDL_Keycode key, TileCallback *callback);
+
+/**
+ * @brief Notifies (calls) all the subscribers for a given key with their args.
+ *
+ * @param key The event keycode subscribers of which to notify.
+ */
+void tile_keyboard_events_notify(KeyEventSubscribers *subscribers,
+                                 SDL_Keycode key);
